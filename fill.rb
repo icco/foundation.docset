7af2189385754d40f066c0ea32fea2f5d99ad031
Nat Welch <nat@natwelch.com>
2013-07-03 07:34:30 -0700

    Fix component listing

--- fill.rb
+++ fill.rb
@@ -9,11 +9,13 @@ def get_header doc
   end
 end
 
+puts "DROP TABLE searchIndex;"
 puts "CREATE TABLE IF NOT EXISTS searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);"
+puts 'CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);'
 
-ARGV.each do |file|
+Dir.glob("Contents/Resources/Documents/components/*html").each do |file|
   doc = Nokogiri::HTML(File.read(file))
+  file_name = File.join("components", File.basename(file))
   header = get_header doc
-
-  puts "INSERT INTO searchIndex VALUES (NULL, '#{header}', 'framework', '#{file}');"
+  puts "INSERT INTO searchIndex VALUES (NULL, '#{header}', 'framework', '#{file_name}');"
 end
